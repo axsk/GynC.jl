a99433480be32ce09f542a1d2ef90e05ff5f1e99
Alex <axsk@users.noreply.github.com>
2015-10-17 19:34:34 +0200

    basic working

--- GynCycle.jl
+++ GynCycle.jl
@@ -1,35 +1,45 @@
-module Jumpheight
-
 using Mamba, Distributions
 include("utils.jl")
 
 g(x,m,s)=1/(s*sqrt(2*pi))*exp(-1/2*((x-m)/s).^2)
-g(x) = g(x,0,10)
-
+rho(x) = g(x,0,10)
+prior(x) = (1/8*(x/40)^2*(1-x/40)^2*(x>0)*(x<40)*30/40
+	+ 7/8*g(x,78,21))
 h(x)=2700*x/470*(1-x/470)^5
-y=[50,72]
+
+y=[150,130] # samples
 yi=y[1]
-model = Model(
-	x = Stochastic(()->Uniform(0,100)), # prior for the weight
-	gauss = Stochastic(x->PDFDistribution(xx->g(y-h(xx)))))
-setsamplers!(model, [NUTS([:gauss])])
-data =  Dict{Symbol,Any}(:y => 72)
-init = [Dict{Symbol,Any}(:x => 0.5, :gauss => 50) for i=1:2]
-res=mcmc(model, data, init, 1000, burnin=200, thin=2, chains=2)
 
+""" Given jumpheights of different persons, return samples of the estimated prior (mean of the posteriors). """
+function priorestimation(y::Vector)
+	res = []
+	samples = [	begin
+			m = Model(
+				x = Stochastic(()->DensityDistribution(x->rho(yi-h(x)))))
+
+			setsamplers!(m, [Slice([:x],[100.0])])
+			data =  Dict{Symbol,Any}()
+			init = [Dict{Symbol,Any}(:x => 60) for i=1:3]
+
+			# cat along right dimension
+			mcmc(m, data, init, 5000, burnin=0, thin=1, chains=3)
+		end for yi in y]
+	return samples
 end
 
+""" Given a prior (weight) and likelihood (height|weight) sample the posterior (height). This simulates measurements across the population."""
+function sampleweights()
+	m = Model(
+		x = Stochastic(()->DensityDistribution(x->prior(x))),
+		y = Stochastic(x ->DensityDistribution(y->rho(y-h(x)))))
 
+	setsamplers!(m, [Slice([:x,:y],[100,100])])
+	setsamplers!(m, [Slice([:x],[1000]), Slice([:y],[1000])])
+	setsamplers!(m, [NUTS([:x,:y])])
+	data =  Dict{Symbol,Any}()
+	init = [Dict{Symbol,Any}(:x => 60, :y => 100) for i=1:3]
 
-#= estimate prior  
-function priorestimate(sims)
-	sum up each sim for l1 integral
-	"weight" the result. TODO: HOW
+	mcmc(m, data, init, 100000, burnin=0, thin=1, chains=3)
 end
 
-not realy sampling the prior now...
-alternative: intrude into model: """
-
-#y = Stochastic(Uniform(y))
-#find way to store integral 
-=#
+sim=sampleweights()
--- utils.jl
+++ utils.jl
@@ -1,11 +1,21 @@
 using Distributions
-import Distributions: length, insupport, _logpdf
-export PDFDistribution
+import Distributions: length, insupport, logpdf
+export DensityDistribution
 
-type PDFDistribution <: ContinuousUnivariateDistribution
-  pdf::Function
+type DensityDistribution <: ContinuousUnivariateDistribution
+  pdff::Function
+
+  function DensityDistribution(f::Any)
+    new(convert(Function, f))
+  end
 end
 
-function _logpdf(d::PDFDistribution, x)
-  log(d.pdf(x))
+function logpdf(d::DensityDistribution, x::Real)
+  log(d.pdff(x))
+end
+
+length(d::DensityDistribution) = 1
+
+function insupport(d::DensityDistribution, x::Real)
+  true
 end
