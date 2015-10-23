aafa28f88d0aae32408220a1071c6b98e2a65e68
Alex <smartmind23@googlemail.com>
2015-10-23 23:25:19 +0200

    refactor utils, added MultivariateDensityDistribution

--- utils.jl
+++ utils.jl
@@ -1,38 +1,39 @@
 using Distributions
-import Distributions: length, insupport, logpdf
-export DensityDistribution
+import Distributions: length, insupport, logpdf, _logpdf
 
-type DensityDistribution <: ContinuousUnivariateDistribution
-  pdff::Function
 
-  function DensityDistribution(f::Any)
-    new(convert(Function, f))
-  end
+type UnivariateDensityDistribution <: ContinuousUnivariateDistribution
+  pdf::Function
+  insupport::Function
 end
 
-function logpdf(d::DensityDistribution, x::Real)
-  log(d.pdff(x))
+type MultivariateDensityDistribution <: ContinuousMultivariateDistribution
+  pdf::Function
+  dim::Integer
+  insupport::Function
 end
 
-length(d::DensityDistribution) = 1
-
-function insupport(d::DensityDistribution, x::Real)
-  #TODO: implement bound support
-  true
+function DensityDistribution(n, f::Function, insupport::Function=((x)->true))
+  # TODO: determine number of arguments
+  n <= 1 ? 
+    UnivariateDensityDistribution  (f, insupport) : 
+    MultivariateDensityDistribution(f, n, insupport)
 end
 
-function gync(yall::Array{Float64,2}, y0::Vector{Float64}, tspan::Vector{Float64}, Parms::Vector{Float64})
+logpdf   (d::UnivariateDensityDistribution, x)   = log(d.pdf(x))
+_logpdf  (d::MultivariateDensityDistribution, x) = log(d.pdf(x))
+insupport(d::UnivariateDensityDistribution, x)   = d.insupport(x)
+insupport(d::MultivariateDensityDistribution, x) = d.insupport(x)
+length   (d::UnivariateDensityDistribution)      = 1
+length   (d::MultivariateDensityDistribution)    = d.dim
+
+
+""" Wrapper to the LIMEX solver for the GynC model 
+solve the model for the times t given initial condition y0 and parameters parms, and store the result in y"""
+function gync!(y::Array{Float64,2}, y0::Vector{Float64}, t::Vector{Float64}, parms::Vector{Float64})
   n = length(y0)
-  m = length(tspan)
-  ccall((:limstep_, "fortran/GynC.so"), Ptr{Array{Float64,2}}, 
-    (Ptr{Float64}, 
-     Ptr{Float64}, 
-     Ptr{Float64}, 
-     Ptr{Int64}, 
-     Ptr{Int64}, 
-     Ptr{Float64}),
-    yall, y0, tspan, &n, &m, Parms)
+  m = length(t)
+  ccall((:limstep_, "fortran/GynC.so"), Void,  
+    (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int64}, Ptr{Int64}, Ptr{Float64}),
+    y, y0, t, &n, &m, parms)
 end
-
-
-  
