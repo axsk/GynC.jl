7ad18a69cd82da0f6bbded1a9f755434c5b7780c
Alex <smartmind23@googlemail.com>
2015-10-30 22:24:51 +0100

    prototype gyncmodel

--- GynCycle.jl
+++ GynCycle.jl
@@ -3,9 +3,9 @@ using MAT, DataFrames
 include("utils.jl")
 
 # indices for measured variables: LH, FSH, E2, P4
-MEASURED = [2,7,24,25]
+const MEASURED = [2,7,24,25]
 # indices for the parameters to be sampled
-SAMPLEPARMS = [4, 6, 10, 18, 20, 22, 26, 33, 36, 39, 43, 47, 49, 52, 55, 59, 65, 95, 98, 101, 103]
+const SAMPLEPARMS = [4, 6, 10, 18, 20, 22, 26, 33, 36, 39, 43, 47, 49, 52, 55, 59, 65, 95, 98, 101, 103]
 
 """ load the patient data and return a vector of Arrays, each of shape 4x31 denoting the respective concentration or NaN if not available """
 function loadpfizer(path = "data/pfizer_normal.txt")
@@ -45,8 +45,7 @@ function test_gync()
 end
 
 """ likelihood (up to proport.) for the parameters given the patientdata """
-function loglikelihood(parms, data, y0)
-  sigma = 1
+function loglikelihood(data::Matrix, parms::Vector, y0::Vector, sigma_rho::Real)
   tspan = Array{Float64}(collect(1:31)) 
   y = gync(y0, tspan, parms)
   
@@ -54,32 +53,51 @@ function loglikelihood(parms, data, y0)
   simdata = y[MEASURED,:]
   
   sre = minimum([squaredrelativeerror(data, translatecol(simdata, transl)) for transl in 0:30])
-  -1/(2*sigma^2) * sre
+  -1/(2*sigma_rho^2) * sre
 end
 
 """ componentwise squared relative difference of two matrices """
-function squaredrelativeerror(data1, data2)
+function squaredrelativeerror(data1::Matrix, data2::Matrix)
   diff = data1 - data2
+  # TODO: divide by data1 or data2?
   rdiff = diff ./ data1
-  sre   = sum(rdiff[!isnan(rdiff)] .^ 2)
+  sre   = sumabs2(rdiff[!isnan(rdiff)])
 end
 
-
-function translatecol(data,transl)
+""" cyclic translation of the columns of `data` by `transl` to the left """
+function translatecol(data::Matrix, transl::Integer)
   hcat(data[:, 1+transl:31], data[:, 1:transl])
 end
 
+""" Return the Bayesian Model with priors y0 ~ Norm(y0), parms' ~ Norm(parms'). Here parms' denotes the sampled parameters, while `parms` are all parameters. """
+function gyncmodel(data::Matrix, parms::Vector, y0::Vector)
+  tparms = copy(parms) # copy for mutating via mergeparms!
+  sigma_y0 = diagm(y0/10)
+  sigma_parms = diagm(parms[SAMPLEPARMS]/10)
+  sigma_rho = length(SAMPLEPARMS) / 10
 
-function patientmodel(data::PatientData, allparms::Vector, y0)
-  allparms = copy(allparms)
-  Model(
-    y0 = Stochastic(MVNormal(y0, sigma)),
-    likelihood = Stochastic(
-      y0 -> LogDensityDisribution(
-        parms->loglikelihood(mergepams!(parms, allparms), data, y0))))
+  m = Model(
+    y0 = Stochastic(length(y0),
+      () -> MVNormal(y0, sigma_y0)),
+    sparms = Stochastic(length(SAMPLEPARMS),
+      () -> MVNormal(allparms[SAMPLEPARMS], sigma_parms)),
+    parms = Logical(length(parms),
+      (sparms) -> mergeparms!(sparms, tparms), false),
+    data = Stochastic(size(data, 1),
+      (y0, parms) -> LogDensityDisribution(data->loglikelihood(data, aparms, y0, sigma_rho))))
+
+  inputs = Dict{Symbol,Any}()
+  inits  = Dict{Symbol,Any}(:y0 -> y0, :parms -> parms, :data -> data)
+  m, inputs, [inits]
 end
 
 function mergeparms!(sampled::Vector, all::Vector)
   all[SAMPLEPARMS] = sampled
   all
+end
+
+function test_mcmc()
+  parms, y0 = loadparms()
+  data = loadpfizer[1]
+  mcmc(gyncmodel(data, parms, y0)..., 1000)
 end
