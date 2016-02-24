352a133083612e727cddc095a6260e2c0badff01
axsk <smartmind23@googlemail.com>
2016-02-24 16:56:16 +0100

    lausannedata: fix reading of persons partial measurements

--- src/data.jl
+++ src/data.jl
@@ -53,12 +53,14 @@ function lausannedata(caseid::Int)
 
   for h in 1:4 
     data = readtable(joinpath(dir,hormonefile[h]), separator=',')
-    y = findfirst(data[:Cas].=="$caseid")
-    for day in 1:32
-      val = data[y,daynames[day]]
-      day = (day-1)%31+1 # limex expects only 31 days 
-      if isa(val, Number) 
-        p[h,day] = val
+    try
+      y = findfirst(data[:Cas].=="$caseid")
+      for day in 1:32
+        val = data[y,daynames[day]]
+        day = (day-1)%31+1 # limex expects only 31 days 
+        if isa(val, Number) 
+          p[h,day] = val
+        end
       end
     end
   end
