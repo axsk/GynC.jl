16401d42d19f132f46563debd325d8ad5419ca0c
axsk <smartmind23@googlemail.com>
2016-03-04 21:14:13 +0100

    bugfixes
    use per-model llh cache
    add stdfactor to gaussianmixture
    start refactor of run.jl
    
    runsim runs and produces varying results :)

--- src/model.jl
+++ src/model.jl
@@ -24,41 +24,48 @@ type ModelConfig
   parms_bound::Vector # upper bound of flat parameter prior
 end
 
-ModelConfig(s::Subject, args...) = ModelConfig(data(s); args...)
+ModelConfig(s::Subject, args...) = ModelConfig(data(s), args...)
 
 ModelConfig(data, sigma_rho=0.1, sigma_y0=1, parms_bound::Real=5) =
   ModelConfig(data, sigma_rho, sigma_y0, parms_bound * refparms)
 
-function gaussianmixture(y::Matrix)
-   covariances = mapslices(std, y, 2) |> vec
+function gaussianmixture(y::Matrix, stdfactor=1)
+   covariances = mapslices(std, y, 2) * stdfactor |> vec
    normals = mapslices(yt->MvNormal(yt, covariances), y, 1) |> vec
    MixtureModel(normals)
 end
 
+# HOTFIX for missing insupport method for MixtureModel
+insupport(d::Distributions.MultivariateMixture, x::AbstractVector) = true
+
 """ construct the mamba model """
 function model(c::ModelConfig)
+  cachedllh = cache(llh,3)
+
   Model(
     logy0 = Stochastic(1,
-      () -> gaussianmixture(log(referencesolution())), false),
+      () -> gaussianmixture(log(referencesolution()), c.sigma_y0),
+      false),
 
-    y0 = Logical(1, (logy0) -> exp(logy0)),
+    y0 = Logical(1,
+      (logy0) -> exp(logy0)),
       
     parms = Stochastic(1,
       () -> UnivariateDistribution[Truncated(Flat(), 0, parbound) for parbound in c.parms_bound]),
       
     data = Stochastic(2,
-      (y0, parms) -> DensityDistribution(
-        size(c.data),
-        data -> cachedllh(data, allparms(parms.value), y0.value, c.sigma_rho),
-        log=true),
+      (y0, parms) -> DensityDistribution(size(c.data),
+                       data -> cachedllh(data, allparms(parms.value), y0.value, c.sigma_rho),
+                       log=true),
       false),
 
-    loglikelihood = Logical((y0, parms, data) -> cachedllh(data.value, parms.value, y0.value, c.sigma_rho))
-    )
+    loglikelihood = Logical(
+      (y0, parms, data) -> cachedllh(data.value, allparms(parms.value), y0.value, c.sigma_rho))
+  )
 end
 
 function referencesolution(resolution=1)
-  sol = gync(refy0, collect(1:resolution:31.), refparms)
+  sol = gync(refy0, collect(1:resolution:31.), allparms(refparms))
   # since we get a (small) negative value for OvF, impeding the log transformation for the prior, set this to the next minimal value
   for i in 1:size(sol,1)
     sol[i, sol[i,:] .<= 0] = minimum(sol[i, sol[i,:] .> 0])
@@ -82,10 +89,6 @@ function llh(data::Matrix{Float64}, parms::Vector{Float64}, y0::Vector{Float64},
   sre = squaredrelativeerror(data, y)
   -1/(2*sigma^2) * sre
 end
-
-""" cached loglikelihood to evade double evaluation """
-# see https://github.com/brian-j-smith/Mamba.jl/issues/68
-cachedllh = cache(llh,3)
 
 """ componentwise squared relative difference of two matrices """
 function squaredrelativeerror(data1::Matrix, data2::Matrix)
--- src/run.jl
+++ src/run.jl
@@ -1,36 +1,50 @@
-function runsim(c, iters, inity0=refy0, initparms=refparms, customtune=nothing; thin=1)
+function runsim(c, iters, inity0=refy0, initparms=refparms; customtune=nothing, thin=1, relprop=0.1)
   m = model(c)
 
-  relprop = 0.1
   nparms = length(inity0) + length(initparms)
   prop = log(1+(relprop^2)) * eye(nparms) # TODO: check
 
-  setsamplers!(m, [AMM([:sparms, :y0], prop, adapt=:all)])
+  setsamplers!(m, [AMM([:parms, :logy0], prop, adapt=:all)])
 
   inp = Dict{Symbol,Any}()
-  inits = [Dict{Symbol,Any}(:logy0 => log(inity0), :sparms => initparms, :data => c.data)]
+  inits = [Dict{Symbol,Any}(:logy0 => log(inity0), :parms => initparms, :data => c.data)]
 
   # TODO: fix this hack
   customtune != nothing && settune!(m, [customtune])
   sim = mcmc(m, inp, inits, iters, verbose=true, chains=1, thin=thin)
 end
 
-function startmcmc(c::ModelConfig, iters::Int, thin::Int=100, path::AbstractString="")
-  sim = runsim(c, refy0, refparms, iters)
+function save(sim, path)
+  if !isfile(path)
+    savenew!(sim, path)
+  else
+    append!(sim, path)
+  end
+end
 
+function savenew!(sim, path)
   mkpath(dirname(path))
   jldopen(path, "w") do j
     hdfchunksize = 1000
     d_create(j.plain, "chains", Float64, ((size(sim.value,1),115,1),(-1,115,-1)), "chunk", (hdfchunksize,115,1))
     j["chains"][:,:,:] = sim.value
     j["tune"] = sim.model.samplers[1].tune
-    j["modelconfig"] = c
-    j["thin"] = thin
+    #j["modelconfig"] = c
+    j["thin"] = step(sim)
   end
-  sim
 end
 
-function continuemcmc(path::AbstractString, iters::Int)
+function append!(sim, path)
+  jldopen(path, "r+") do j
+    s = size(j["chains"])
+    set_dims!(j.plain["chains"], (s[1]+size(sim.value,1), s[2], s[3]))
+    j["chains"][s[1]+1:end, :, :] = sim.value
+    delete!(j["tune"])
+    j["tune"] = sim.model.samplers[1].tune
+  end
+end
+
+function load(path)
   local last, tune, thin, c
 
   jldopen(path, "r") do j
@@ -43,16 +57,7 @@ function continuemcmc(path::AbstractString, iters::Int)
   lasty0 = last[1, 83:115, chain] |> vec
   lastthetha = last[1, 1:82, chain] |> vec
 
-  sim = runsim(c, lasty0, lasttheta, iters, thin, tune)
-
-  jldopen(path, "r+") do j
-    s = size(j["chains"])
-    set_dims!(j.plain["chains"], (s[1]+size(sim.value,1), s[2], s[3]))
-    j["chains"][s[1]+1:end, :, :] = sim.value
-    delete!(j["tune"])
-    j["tune"] = sim.model.samplers[1].tune
-  end
-  sim
+  ModelChain()
 end
 
 nsamples(path) = jldopen(j->size(j["chains"],1), path, "r")
