225debc49f18cc5f890fb88e49cfc4dd52fa791a
axsk <smartmind23@googlemail.com>
2016-04-08 11:24:09 +0200

    add vectorized versions of the hyperposterior
    move / comment some lines
    add todos

--- src/reweight.jl
+++ src/reweight.jl
@@ -3,29 +3,22 @@
 type WeightedChain
   chain::AbstractMatrix
   weights::AbstractVector
-  likelihoods::AbstractMatrix
+  likelihoods::AbstractMatrix # row = param, col = subject
 end
+
+
+### WeightedChain constructors for the GynC model
+
+# TODO: move this section to model.jl
 
 WeightedChain(c::Vector, w, l) = WeightedChain(reshape(c,length(c),1), w, l)
 
-function WeightedChain(chain::Matrix, data::Vector{Matrix}, sigma::Real)
-  WeightedChain(chain, ones(size(chain, 1)), likelihoods(chain, data, sigma))
+" construct the WeightedChain computing the likelihoods for the given `samples` (row = sample, col = sampledparam) given `datas` with error `sigma` "
+function WeightedChain(chain::Matrix, datas::Vector{Matrix}, sigma::Real)
+  WeightedChain(chain, ones(size(chain, 1)), likelihoods(chain, datas, sigma))
 end
 
-# TODO: normalisation / uniform scaling over all persons? check importance / necessity for other steps
-function likelihood(data::Matrix, sample::Vector, sigma::Real)
-  parms, y0 = sampletoparms(sample)
-  exp(llh(data, parms, y0, sigma))
-end
-
-function sampletoparms(sample::Vector)
-  np = length(sampledinds)
-  allparms = allparms(sample[1:np])
-  y0 = sample[np+1:end]
-  allparms, y0
-end
-
-""" compute the likelihood matrix for given chains, data, sigma) """
+" compute the likelihood matrix for given chains, data, sigma) "
 function likelihoods(chain::AbstractMatrix, data::Vector{Matrix}, sigma::Real)
   K = size(chain, 1)
   M = length(data)
@@ -37,6 +30,24 @@ function likelihoods(chain::AbstractMatrix, data::Vector{Matrix}, sigma::Real)
   end
   likelihoods
 end
+
+# TODO: base computation on model
+" compute the likelihoods of the `sample` for the given `data` with error `sigma` "
+function likelihood(data::Matrix, sample::Vector, sigma::Real)
+  parms, y0 = sampletoparms(sample)
+  exp(llh(data, parms, y0, sigma))
+end
+
+" given a sample, extend to all model parameters " 
+function sampletoparms(sample::Vector)
+  np = length(sampledinds)
+  allparms = allparms(sample[1:np])
+  y0 = sample[np+1:end]
+  allparms, y0
+end
+
+
+### Old reweighting, using the non-orthogonal projection ###
 
 " reweight the given `WeightedChain` and return a `Dict` with the iterations given in `range` "
 function reweight(c::WeightedChain, range)
@@ -50,13 +61,14 @@ function reweight(c::WeightedChain, range)
   res
 end
 
-
 " reweight the given `WeightedChain` according to its `likelihoods` "
 reweight!(c::WeightedChain) = reweight!(c.weights, c.likelihoods)
 
 function reweight!(w::DenseVector, L::DenseMatrix)
   K = size(L,1)
   M = size(L,2)
+
+  #= performing slower?
   norm = Array{Float64}(M)
   @inbounds for m=1:M
     s = 0.
@@ -65,6 +77,9 @@ function reweight!(w::DenseVector, L::DenseMatrix)
     end
     norm[m] = s # rho(z_m)
   end
+  =#
+
+  norm = L' * w
 
   @inbounds for k=1:K
     s = 0.
@@ -76,7 +91,28 @@ function reweight!(w::DenseVector, L::DenseMatrix)
   w
 end
 
-### simplex projection ###
+
+### Maximal Likelihood / Posterior for the Prior ###
+
+A_comprehension(w,L) = product([sum([L[k,m]*w[k] for k=1:length(w)]) for m=1:size(L,2)])
+dA_comprehension(w,L) = [A(w,L) * sum([L[j,m] / sum([L[k,m]*w[k] for k in 1:length(w)]) for m in 1:size(L,2)]) for j in 1:length(w)]
+
+" posterior for the priors evaluated at w"
+A(w::Vector, L::Matrix) = prod(L'*w) :: Real
+
+function dA(w::Vector, L::Matrix)
+  norms = L'*w
+  A = prod(norms)
+  inv = 1 ./ norms
+  (L * inv) :: Vector
+end
+
+" gradient ascend of A(w) projected onto the simplex, returning the next step for stepsize h " 
+gradient_simplex(c::WeightedChain, h::Real) = gradient_simplex(c.weights, c.likelihoods, h)
+gradient_simplex(w,L,h) = projectsimplex!(w + dA(w, L) * h) 
+
+
+### simplex projection algorithms ###
 
 # c.f. https://www.gipsa-lab.grenoble-inp.fr/~laurent.condat/publis/Condat_simplexproj.pdf"
 
