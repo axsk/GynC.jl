8be53306641e4b0cd9e62a28f225f63deb051490
axsk <smartmind23@googlemail.com>
2016-04-28 12:13:48 +0200

    prototype entropy

--- src/reweight.jl
+++ src/reweight.jl
@@ -50,6 +50,7 @@ function sampletoparms(sample::Vector)
 end
 
 
+### Self-consistency iteration / EM-Algorithm
 ### Old reweighting, using the non-orthogonal projection ###
 
 " reweight the given `WeightedChain` according to its `likelihoods` "
@@ -86,10 +87,7 @@ end
 ### Maximal Likelihood / Posterior for the Prior ###
 # TODO: use nonlinear optimizer
 
-A_comprehension(w,L) = product([sum([L[k,m]*w[k] for k=1:length(w)]) for m=1:size(L,2)])
-dA_comprehension(w,L) = [A(w,L) * sum([L[j,m] / sum([L[k,m]*w[k] for k in 1:length(w)]) for m in 1:size(L,2)]) for j in 1:length(w)]
-
-" posterior for the priors evaluated at w"
+" posterior for the priors evaluated at w "
 A(w::Vector, L::Matrix) = prod(L'*w) :: Real
 
 function dA(w::Vector, L::Matrix)
@@ -98,6 +96,15 @@ function dA(w::Vector, L::Matrix)
   inv = 1 ./ norms
   (L * inv) :: Vector
 end
+
+" - /int pi(x) log(pi(x)) dx "
+function H(pi)
+  # TODO: make this integral work for non-uniform grid for the pi
+  -dot(x, log(x))
+end
+
+#function PhiH = log(A) - H
+    
 
 " gradient ascend of A(w) projected onto the simplex, returning the next step for stepsize h " 
 function gradient_simplex!(c::WeightedChain, h::Real)
