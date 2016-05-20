b00177ea5e53e4d65035acbdc998ae48c69116c7
axsk <smartmind23@googlemail.com>
2016-05-20 17:30:56 +0200

    fix tests

--- examples/gc/runtests.jl
+++ examples/gc/runtests.jl
@@ -2,9 +2,9 @@ sim = mcmc(Config(Lausanne(1)), 10)
 
 tmp = tempname() * ".jld"
 
-s = batch(tmp, batchiters=10, maxiters=20, config=Config(Lausanne(1)))
+s = batch(tmp, batchiters=10, maxiters=20, config=Config(Lausanne(1), thin=2))
 s = load(tmp)
 
-@assert size(s.samples) == (10, 115, 1)
+@assert size(s.samples) == (10, 115)
 
 rm(tmp)
