a68940f413afa3bf339b5ea172b7a301f26d46b1
axsk <smartmind23@googlemail.com>
2016-05-20 17:11:13 +0200

    move tests to gc/

--- examples/gc/gync.jl
+++ examples/gc/gync.jl
@@ -5,12 +5,23 @@ using Requires
 
 import Sundials
 
+
 export Lausanne, Pfizer
 export Config
 export mcmc, batch
 export load, save
 
+
 include("constants.jl")
+
+
+type Subject
+  data::Array{Float64}
+  id::Any
+end
+
+data(s::Subject) = s.data
+
 
 type Config
   data::Matrix      # measurements
@@ -29,6 +40,7 @@ Config(s::Subject; args...) = Config(data(s); args...)
 Config(data; sigma_rho=0.1, sigma_y0=1, parms_bound::Real=5, relprop=0.1, thin=1, init=refinit) =
   Config(data, sigma_rho, sigma_y0, parms_bound * refparms, relprop, thin, init)
 
+
 type Sampling
   samples::Array
   logprior::Vector
@@ -37,12 +49,6 @@ type Sampling
   config::Config
 end
 
-type Subject
-  data::Array{Float64}
-  id::Any
-end
-
-data(s::Subject) = s.data
 
 include("data/lausanne.jl")
 include("data/pfizer.jl")
--- test/io.jl
+++
@@ -1,8 +0,0 @@
-tmp = tempname() * ".jld"
-
-s = batch(tmp, batchiters=10, maxiters=20, config=GynCConfig(Lausanne(1)))
-s = load(tmp)
-
-@assert size(s.samples) == (10, 115, 1)
-
-rm(tmp)
