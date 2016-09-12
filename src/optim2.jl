ffbb023995a5bd9ab5a245927ef393c38ecb2b0e
axsk <smartmind23@googlemail.com>
2016-09-12 12:04:15 +0200

    complete optim2.jl

--- src/optim2.jl
+++ src/optim2.jl
@@ -1,7 +1,5 @@
-# given X-samples `x`, the forwardd map `phi`:X->Z, and the error density `err`, compute the pairwise likelihood matrix Lz, corresponding to L(x|z)
-
+"compute the pairwise likelihood matrix Lz, with Lz_ij = L(x_i|z_j)"
 function Lz(z::Vector, err::Function)
-  z = map(phi, xs)
   n = length(z)
   L = Array{Float64}(n,n)
   for i=1:n
@@ -12,28 +10,32 @@ function Lz(z::Vector, err::Function)
   L
 end
 
-# compute the prior predictive distribution entropy
-# w - the weights of the samples
-# Lz - the z likelihood matrix
-
-Hz(pi, phi, err) = Hz(pi, Lz(pi, phi, err))
-
+"compute the prior predictive distribution entropy
+ w - the weights of the samples
+ Lz - the z likelihood matrix (see above)"
 function Hz(w::Vector, Lz::Matrix)
   rhoz = Lz * w
   rhoz = rhoz / sum(rhoz)
   -dot(log(rhoz), w)
 end
 
-
-phi(x::Vector) = forwardsol(x, 1:31)
-phi(x::Matrix) = [phi(x[i,:]) for i in 1:size(x, 1)]
-
-function obj(x::Matrix, w::Vector, datas::Vector, sigma::Real)
+"construct the objective function w -> L + Hz"
+function hzobj(x::Matrix, datas::Vector, err::Function)
   z = phi(x)
-  L = [llh(z, data, sigma) for z in z, data in datas]
-  err(z1, z2) = exp(llh(z1, z2, sigma))
-  A(w,L) + Hz(w, Lz(z, err))
+  L = [err(z, data) for z in z, data in datas]
+  Lz = Lz(z, err)
+  function o(w)
+    A(w,L) + Hz(w, Lz)
+  end
 end
 
-# log L(z|data), i.e. the measurement error, up to a constant
-llh(z, data, sigma) = znorm(z, data) / (2*sigma^2)
+hzobj(x::Matrix, datas::Vector, sigma::Real) = hzobj(x, datas, (a, b) -> rho_e(a, b, sigma))
+
+### Model specific ###
+
+# forward solution
+phi(x::Vector) = forwardsol(x, 1:31)[:, measuredinds]
+phi(x::Matrix) = [phi(x[i,:] |> vec) for i in 1:size(x, 1)]
+
+# log L(z|data), i.e. the measurement error, up to normalization
+rho_e(z1, z2, sigma) = exp(-znorm(z1, z2) / (2*sigma^2))
