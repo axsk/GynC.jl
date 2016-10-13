97f67a6a1f35f1da77330acba11b65eb668ecddc
axsk <smartmind23@googlemail.com>
2016-10-13 12:42:30 +0200

    add movefromboundary() to gradientascent()
    since the projection might project the ga steps onto the boundary we ran into difficulties with the gradient of the entropy, becoming infinite.
    
    resolve this by effectively reducing the stepsize to land inside the
    simplex.

--- src/federn/utils.jl
+++ src/federn/utils.jl
@@ -8,8 +8,19 @@ function gradify(f, x)
     end
 end
 
-function gradientascent(f, w0, n, h, projection)
+function gradientascent(f, w0, n, h, projection=projectsimplex)
     df = gradify(f, w0)
-    iter(w) = projection(w + h * df(w)[2])
+    iter(w) = movefromboundary(w, projection(w + h * df(w)[2]))
     collect(take(iterate(iter, w0), n))
+end
+
+" given two points on the simplex `wold` and `wnew`,
+check whether `wnew` lies on the boundary and if so,
+move it to linear combination of `wnew` and `wold` "
+function movefromboundary(wold, wnew, relstep=1/2)
+    if any(wnew .== 0)
+        (1-relstep)*wold + relstep*wnew
+    else
+        wnew
+    end
 end
