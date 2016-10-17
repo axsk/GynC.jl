87d9e6b0035cd09a80f094562fae9f1306c49d56
axsk <smartmind23@googlemail.com>
2016-10-18 01:02:05 +0200

    add hkl functional

---
+++ src/optim3.jl
@@ -0,0 +1,13 @@
+# compute the integral \int p(z|pi) D_KL(pi(x), p(x|pi)) dz
+# L[l,k] = L(zl | xk), zl = phi(xk)+el
+function HKL(w, L, wz=w)
+  #@assert length(w) == size(Ldz, 2)
+  evidences = L * w
+  h = 0.
+  for k = 1:size(L, 2)
+    for l = 1:size(L, 1)
+      h += wz[l] * L[l, k] * w[k] / evidences[l] * log( L[l,k] / evidences[l])
+    end
+  end
+  h / size(Lzz, 1) / size(Lzz, 2)
+end
